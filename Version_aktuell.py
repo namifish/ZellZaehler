e5a183edb5d843ecfc6b8e8b82c70cb2f805664a
namifish <152783325+namifish@users.noreply.github.com>
2024-05-18 23:06:55 +0200

    rechtschreibefehler

--- Version_aktuell.py
+++ Version_aktuell.py
@@ -289,9 +289,9 @@ else:
         Diese App wurde in erster Linie für das Hämatologie Praktikum an der ZHAW erschaffen. Sie dient als Stütze für die Zählung von Leukozyten.
 
         **Funktionen:**
-        - **Probenummer eingeben**: Geben eine eindeutige Probenummer ein, um eine neue Zählung zu starten.
-        - **Zählen**: Führen die Zählungen durch, indem du die entsprechenden Knöpfe drückst.
-        - **Zelle hinzufügen**: Klicken auf diesen Knopf, um die unteren drei Knöpfe umzubenennen.
+        - **Probenummer eingeben**: Gib eine eindeutige Probenummer ein, um eine neue Zählung zu starten.
+        - **Zählen**: Führe die Zählungen durch, indem du die entsprechenden Knöpfe drückst.
+        - **Zelle hinzufügen**: Klicke auf diesen Knopf, um die unteren drei Knöpfe umzubenennen.
         - **Korrigieren**: Ermöglicht das manuelle Korrigieren der Zählerstände. Bitte achte darauf, dass durch die Korrektur nicht mehr als 100 Zellen insgesamt gezählt werden. Im Notfall kannst du den letzten Schritt rückgängig machen.
         - **Rückgängig**: Macht den letzten Zählungsschritt rückgängig.
         - **Zählung zurücksetzen**: Setzt alle Zählerstände auf Null zurück.
